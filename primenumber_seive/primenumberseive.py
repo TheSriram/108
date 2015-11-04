027fc2bf92f07101079a6b5148f39fde244491cc
Sriram Madapusi Vasudevan <sriram@klusterkloud.com>
2015-11-04 16:51:22 -0500

    Update primenumberseive.py

--- primenumber_seive/primenumberseive.py
+++ primenumber_seive/primenumberseive.py
@@ -1,11 +1,13 @@
 def primenumberseive(n):
 
     total_list = [i for i in range(2, n)]
+    prime_list = [i for i in range(2, n)]
     for i in range(2, int(math.sqrt(n))):
         count = 0
-        while ((i * i) + (i*(count+1))) <n:
-            total_list[(i * i) + (i*count)] = 0
+        while ((i * i) + (i*(count))) <n:
+            try:
+                prime_list.remove((i * i) + (i*count))
+            except ValueError:
+                pass
             count = count + 1
-    primes = [elem for elem in total_list if elem!=0]
-    return primes
-
+    return prime_list
